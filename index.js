45f602879c0953eb64810d05e40a93f88a690aff
Raquel Perez <137131652+raquelmpg@users.noreply.github.com>
2023-07-03 23:14:04 -0300

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById('miBoton');
+function hadfleClick(){
+    alert('¡Hola! Has hecho ckic en el botón.');}
+
+miBoton.addEventListener("click", function(){
+    alert("¡Haz Hecho clic en el botón!")
+});
